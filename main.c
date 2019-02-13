1fd2273975f99cb888723abfe8f1157746af9bb4
Benjamin Lewis <blewis@lacreativeria.com>
2019-02-12 20:44:20 -0600

    01-TrackSysCalls

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 		printf("Main running\n");
+		system("dtruss -c ./program");
+		system("strace -c ./program");
+		printf("Main ending\n");
 		return 0;
 }
+
